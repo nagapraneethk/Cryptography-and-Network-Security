120ec6582084facad2d21828d4704e10fd8a436f
Naga Praneeth <nagapraneethkandikandi@gmail.com>
2025-03-11 22:07:57 +0530

    Lab Task 06

--- Lab_Task_06/q1.py
+++ Lab_Task_06/q1.py
@@ -5,14 +5,11 @@ b = 135481653441354138548413384135
 addition = a + b
 print(f"Addition: {addition}")
 
-
 subtraction = a - b
 print(f"Subtraction: {subtraction}")
 
-
 multiplication = a * b
 print(f"Multiplication: {multiplication}")
-
 
 division = a / b
 print(f"Division: {division}")
--- Lab_Task_06/q2.py
+++ Lab_Task_06/q2.py
@@ -1,6 +1,20 @@
+import math
+
 a = 182841384165841685416854134135
 b = 135481653441354138548413384135
 
 modular_div= a % b
 print(f"Modular Division: {modular_div}")
 
+
+division_int = a // b
+gcd_ab = math.gcd(a, b)
+print(f"integer Division: {division_int}")
+
+modinverse = 0
+try:
+    modinverse = pow(a, -1, b)
+except ValueError:
+    print("NO MOD INVERSE FOUND FOR THIS")
+
+print(f"Modulo inverse Division: {modinverse}")
